c95e936207a4eaadee1e4f582c6f4f684e8900c6
ruhulbdgit <91396640+ruhulbdgit@users.noreply.github.com>
2023-08-04 12:40:48 +0600

    some style for form

--- views/user/add.php
+++ views/user/add.php
@@ -1,24 +1,18 @@
+<form action="process_registration.php" method="post">
+    <label for="first_name">First Name:</label>
+    <input type="text" class="nameInput" id="first_name" name="first_name" required><br>
 
-    <form action="process_registration.php" method="post">
-        <label for="first_name">First Name:</label>
-        <input type="text" id="first_name" name="first_name" required><br>
+    <label for="last_name">Last Name:</label>
+    <input type="text" class="emailInput" id="last_name" name="last_name" required><br>
 
-        <label for="last_name">Last Name:</label>
-        <input type="text" id="last_name" name="last_name" required><br>
+    <label for="email">Email:</label>
+    <input type="email" id="email" name="email" required><br>
 
-        <label for="email">Email:</label>
-        <input type="email" id="email" name="email" required><br>
+    <label for="password">Password:</label>
+    <input type="password" id="password" name="password" required><br>
 
-        <label for="password">Password:</label>
-        <input type="password" id="password" name="password" required><br>
+    <label for="registration_date">Registration Date:</label>
+    <input type="date" id="registration_date" name="registration_date" required><br>
 
-        <label for="registration_date">Registration Date:</label>
-        <input type="date" id="registration_date" name="registration_date" required><br>
-
-        <input type="submit" value="Register">
-    </form>
-
-
-
-
-
+    <input type="submit" value="Register">
+</form>
