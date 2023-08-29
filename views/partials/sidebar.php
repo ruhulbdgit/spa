3264eeb71a3336bd85c422ed3a4444864000fe13
ruhulbdgit <91396640+ruhulbdgit@users.noreply.github.com>
2023-08-29 14:12:12 +0600

    added add page & list of pages

--- views/partials/sidebar.php
+++ views/partials/sidebar.php
@@ -7,3 +7,12 @@
   <a href="?page=user&todo=list">
       <h2>list User</h2>
   </a>
+  <br>
+  <br>
+  <br>
+  <a href="?page=user&todo=list">
+      <h2>Add Pages</h2>
+  </a>
+  <a href="?page=user&todo=list">
+      <h2>List of pages</h2>
+  </a>
